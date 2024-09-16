e8a6695c2eae790b257a1860e13c9e4297162d89
sahil singh <sahilsingh1413@gmail.com>
2024-09-16 21:16:11 +0530

    Fixed Database connection

--- backend/Config/dbConn.js
+++ backend/Config/dbConn.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery' , false);
 const connectionToDb = async()=>{
 
     try{
-        const {connection} = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/travelBooking");
+        const {connection} = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://sahilsinghmongotest:sahilsingh72@cluster0.xw4zw.mongodb.net/travelBooking");
     
         if(connection){
             console.log("Database successfully connected on the" , connection.host);
